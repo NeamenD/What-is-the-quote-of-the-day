a955d0e8ca9b9d8e4c81fdfc2628f2ee15d3dcf8
NeamenD <neamenbirhanu@gmail.com>
2023-08-24 13:44:14 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// in this project every time a user rans a project they get a new randomized output
+const generate_random = (num)=>{
+    return Math.floor(Math.random(num) *2)
+
+}
+string_arr = []
+console.log(generate_random(num))
