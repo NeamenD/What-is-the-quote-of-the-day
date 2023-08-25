484d4e9f0e7c23b41ed01303d43a6e15270f5a87
NeamenD <neamenbirhanu@gmail.com>
2023-08-25 10:39:53 -0600

    working version

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 // in this project every time a user rans a project they get a new randomized output
-// const generate_random = (num)=>{
-//     return Math.floor(Math.random(num) *2)
-
-// }
-// string_arr = []
-// console.log(generate_random(num))
-
 import myArray from './data_set.js';
 function Get_random_Array(myArray){
     const randomindex = Math.floor(Math.random() * myArray.length)
@@ -20,9 +13,4 @@ const authorName = document.getElementById("authorName")
 styledButton.addEventListener('click', ()=>{
     const randomQuote_author = Get_random_Array(myArray)
     cardText.textContent = `" ${randomQuote_author.quote} "`
-    authorName.textContent = `By - ${randomQuote_author.author}`
-
-})
-
-
-// console.log(randomQuote_author.quote, randomQuote_author.author)
+    authorName.textContent = `By - ${randomQuote_author.author}`})
